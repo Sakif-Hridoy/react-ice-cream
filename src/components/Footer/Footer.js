dd51d0478292bfefd2473338ef45ed238592ddf4
Sakif <sakifworkspace@gmail.com>
2022-02-01 13:30:28 +0600

    join() method,take two or more classes rules

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+        <div class={["container",classes.container].join()}>
+          <div>
+            Copyright &copy;2020.
+          </div>
+          <div class="textRight">
+            Built with <span class="red">&hearts;</span> by
+            <a
+              href="https://reactjs.org/docs/create-a-new-react-app.html"
+              target="_blank"
+              >Create React App</a
+            >
+          </div>
+        </div>
+      </footer>
+  )
+};
+
+export default Footer;
+
