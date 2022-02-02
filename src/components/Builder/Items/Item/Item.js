e09a69542266c79d8e0727129959974a4ecb7c1f
Sakif <sakifworkspace@gmail.com>
2022-02-02 23:24:46 +0600

    Create Items,Item components

---
+++ src/components/Builder/Items/Item/Item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Item.module.css';
+
+const Item = () => {
+  return (
+    <li class={classes.item}>
+    <span>Vanilla</span>
+    <span class={classes.quantity}>2</span>
+    <div class="right">
+      <button type="button" class={[classes.plus,'rounded'].join(' ')}>+</button>
+      <button type="button" class={[classes.minus,'rounded'].join(' ')}>-</button>
+    </div>
+  </li>
+  );
+};
+
+export default Item;
---
+++ src/components/Builder/Items/Items.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Items = () =>{
+    return(
+        <div>
+            <ul>
+                {/* item */}
+            </ul>
+        </div>
+    )
+}
+
+export default Items;
