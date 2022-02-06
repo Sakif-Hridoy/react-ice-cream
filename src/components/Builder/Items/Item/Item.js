305c9397e31a8ff5c25b5862e948f8ea6acef6cf
Sakif <sakifworkspace@gmail.com>
2022-02-06 16:50:41 +0600

    import lodash library from react and pass it to scoopsByCount

--- src/components/Builder/Items/Item/Item.js
+++ src/components/Builder/Items/Item/Item.js
@@ -2,13 +2,13 @@ import React from 'react';
 import classes from './Item.module.css';
 import {countBy} from 'lodash';
 
-const Item = ({name,add,remove,scoops}) => {
+const Item = ({name,add,remove,scoops={}}) => {
   const scoopsByCount = countBy(scoops);
   console.log(scoopsByCount);
   return (
     <li class={classes.item}>
     <span>{name}</span>
-    <span class={classes.quantity}>{scoopsByCount[name]}</span>
+    {scoopsByCount[name]>= 0 ? (<span class={classes.quantity}>{scoopsByCount[name]}</span>):null}
     <div class="right">
       <button onClick={add.bind(this, name)} type="button" class={[classes.plus,'rounded'].join(' ')}>+</button>
       <button onClick={remove.bind(this, name)} type="button" class={[classes.minus,'rounded'].join(' ')}>-</button>
